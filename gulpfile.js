cafb07e5682d3bf77d552f9fa2ba43ff8f12fefa
Brian Rotich <brian.rotich@andela.com>
2017-07-12 08:27:02 +0300

    (chore) fix code climate issues

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,22 @@
 const gulp = require('gulp');
-const codacy= require('gulp-codacy');
-const eslint = require('gulp-eslint');
+const codacy = require('gulp-codacy');
 const istanbul = require('gulp-istanbul');
 const mocha = require('gulp-mocha');
 
-gulp.task('coverage:setup', () => {
-  return gulp.src(['mock-server/*.js'])
+gulp.task('coverage:setup', () => gulp.src(['mock-server/*.js'])
     .pipe(istanbul({ includeUntested: true }))
-    .pipe(istanbul.hookRequire());
-});
+    .pipe(istanbul.hookRequire()));
 
-gulp.task('test:backend', ['coverage:setup'], () => {
-  return gulp.src(['test/**/*.js'])
+gulp.task('test:backend', ['coverage:setup'], () => gulp.src(['test/**/*.js'])
     .pipe(mocha())
     .on('error', process.exit.bind(process, 1))
     .pipe(istanbul.writeReports({
       dir: './coverage',
-    }));
-});
+    }))
+);
 
 gulp.task('test', ['test:backend']);
 
-gulp.task('codacy-reporter', () => {
-  gulp.src(['./coverage/lcov.info'])
-    .pipe(codacy());
-});
+gulp.task('codacy-reporter', () => gulp.src(['./coverage/lcov.info'])
+    .pipe(codacy())
+);
